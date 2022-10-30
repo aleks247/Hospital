5d46b7bc81cda64f19e24b526308808668fec63f
berki0 <107307903+berki0@users.noreply.github.com>
2022-10-30 14:20:03 +0200

    Add files via upload

---
+++ src/RecordedHoursData.java
@@ -0,0 +1,9 @@
+public class RecordedHoursData {
+    private int appointmentId;
+    private int patient_id;
+    private String typeOfExamination;
+    private String date;
+    private String time;
+    private int doctorId;
+
+}
