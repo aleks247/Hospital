c9cf192081fde6a24f44886dfcfe1834fc655ae2
berki0 <107307903+berki0@users.noreply.github.com>
2022-11-17 21:25:00 +0200

    add text reader + text to Object

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +11,49 @@ public class Main {
         System.out.println("Doctor -> 1 ");
         System.out.println("Patient -> 2");
         System.out.print("Choose your role : ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                checkingAvailableDoctors();
+                break;
+            case 2:
+                patientEntrance();
+                break;
+            case 0:
+                System.out.println("exit");
+                break;
+            default:
+                System.out.println("Please enter a valid option ! ");
+                printMenu();
+                break;
+        }
     }
 
+    public static void checkingAvailableDoctors() {
+        File doctorsFile = new File("doctors.txt");
+        try {
+            FileReader fileReader = new FileReader(doctorsFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(",");
+                doctorspurvired(words);
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+public static void doctorspurvired(String[]words){
+
+    for (int i = 0; i < words.length; i++) {
+        System.out.println(words[i]);
+       // Doctor doctor=new Doctor((Integer.getInteger(words[i]),words[i],words[i],words[i],words[i]);
+
+
+    }
+
+}
     public static void entryForDoctors() {
 
     }
@@ -19,24 +63,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        while (true) {
-            printMenu();
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    entryForDoctors();
-                    break;
-                case 2:
-                    patientEntrance();
-                    break;
-                case 0:
-                    System.out.println("exit");
-                    break;
-                default:
-                    System.out.println("Please enter a valid option ! ");
-                    break;
-            }
-        }
+        printMenu();
+
     }
 }
 
