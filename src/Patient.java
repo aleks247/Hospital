c0a6d9e0926947be6b114f1aa70951dbc9462999
berki0 <107307903+berki0@users.noreply.github.com>
2022-11-22 21:12:01 +0200

    creatingObjectsFromReadyInformation for doctor Patient and Appointment

--- src/Patient.java
+++ src/Patient.java
@@ -12,6 +12,22 @@ public class Patient implements Login{
         this.age = age;
     }
 
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public void seeAllHours(){
 
     }
